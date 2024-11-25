4c030899221ec05a3e4a806f2101680c4bbb3522
Amalsha <amalsha2001@gmail.com>
2024-11-25 15:54:49 +0530

    Initial backend commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./index.css";
-import HomePage from "./pages/home/homepage";
+import HomePage from "./pages/home/homepage.jsx";
 import JobPage from "./pages/job/jobpage";
-
 import RootLayout from "./layouts/root.layout";
 import SignInPage from "./pages/sign-in.page";
 import SignUpPage from "./pages/sign-up.page";
 import MainLayout from "./layouts/main.layout";
+import AdminMainLayout from "./layouts/admin.layout";
+import AdminJobPostsPage from "./pages/admin/jobPosts/admin-job-posts.page";
+import AdminJobCreatePage from "./pages/admin/createJob/job-create.page";
+import AdminJobPage from "./pages/admin/job/admin-job.page";
+import AdminJobApplicationPage from "./pages/admin/jobApplication/admin-job-application.page";
 
 const router = createBrowserRouter([
   {
@@ -24,6 +28,28 @@ const router = createBrowserRouter([
           {
             path: "/job/:id",
             element: <JobPage />,
+          },
+        ],
+      },
+      {
+        path: "admin",
+        element: <AdminMainLayout />,
+        children: [
+          {
+            path: "jobs",
+            element: <AdminJobPostsPage />,
+          },
+          {
+            path: "job/create",
+            element: <AdminJobCreatePage />,
+          },
+          {
+            path: "job/:id",
+            element: <AdminJobPage />,
+          },
+          {
+            path: "job/:id/application/:applicationId",
+            element: <AdminJobApplicationPage />,
           },
         ],
       },
--- src/pages/home/components/Hero.jsx
+++ src/pages/home/components/Hero.jsx
@@ -2,7 +2,8 @@ function Hero() {
   return (
     <section className="py-32 flex justify-center items-center rounded-xl shadow-sm shadow-gray-900 border-white hero">
       <div>
-        <h1 className="text-white">Find your dream job</h1>
+      <h1 className="text-white text-4xl md:text-5xl lg:text-6xl font-bold drop-shadow-lg mb-4">
+      Find your dream job</h1>
       </div>
     </section>
   );
